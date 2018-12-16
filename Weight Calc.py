217070838c3bc6880d5c95b090eadf2545a9914e
PapaElGunmen <45818936+PapaElGunmen@users.noreply.github.com>
2018-12-16 09:40:16 +0100

    New files

---
+++ Weight Calc.py
@@ -0,0 +1,14 @@
+a=input("")
+data=open("data.txt","r")
+checker=false
+
+#read value of a
+While checker == false:
+	if a==data():
+		checker=true
+		#calculate weight
+		weight=
+	else
+	#read next value
+
+#QUERY SQL
