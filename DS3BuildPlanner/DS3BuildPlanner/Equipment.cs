b1815f4715449a0231e8216d10fb248ebf9914cd
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-30 20:30:48 +0200

    Update Equipment.cs

--- DS3BuildPlanner/DS3BuildPlanner/Equipment.cs
+++ DS3BuildPlanner/DS3BuildPlanner/Equipment.cs
@@ -8,5 +8,35 @@ namespace DS3BuildPlanner
 {
     class Equipment
     {
+        private static List<Item> equipment;
+        private static Equipment instance;
+        private Equipment()
+        {
+            equipment = new List<Item>();
+        }
+        public static Equipment getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Equipment();
+            }
+            return instance;
+
+        }
+        public void addItem(Item i)
+        {
+            equipment.Add(i);
+        }
+        public void removeItem(Item i)
+        {
+            if (equipment.Contains(i))
+            {
+                equipment.Remove(i);
+            }
+            else
+            {
+               throw new ItemNotFound();
+            }
+        }
     }
 }
