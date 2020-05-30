2fe1ad3472fc4aeeecad9f07ce379f13224e598c
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-30 14:20:03 +0200

    Update Form1.Designer.cs

--- DS3BuildPlanner/DS3BuildPlanner/Form1.Designer.cs
+++ DS3BuildPlanner/DS3BuildPlanner/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.testcb = new MaterialSkin.Controls.MaterialComboBox();
             this.FatRollbtn = new MaterialSkin.Controls.MaterialRadioButton();
             this.MediumRollbtn = new MaterialSkin.Controls.MaterialRadioButton();
@@ -39,22 +40,40 @@
             this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
             this.materialTabControl2 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.Info = new System.Windows.Forms.TabPage();
+            this.materialListView1 = new MaterialSkin.Controls.MaterialListView();
+            this.materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.materialComboBox1 = new MaterialSkin.Controls.MaterialComboBox();
             this.materialSwitch1 = new MaterialSkin.Controls.MaterialSwitch();
             this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
-            this.Info = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.materialButton2 = new MaterialSkin.Controls.MaterialButton();
-            this.materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PhysicalAbsortion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MagicAbsortion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FireAbsortion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.materialTabControl3 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.materialTabSelector2 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.LightningAbsortion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DarkAbsortion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BleedResistance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PoisonResistance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FrostResistance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CurseResistance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Poise = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Weight = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PoiseWeightRatio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.materialTabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.materialTabControl2.SuspendLayout();
             this.tabPage3.SuspendLayout();
-            this.tabPage5.SuspendLayout();
             this.Info.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.materialTabControl3.SuspendLayout();
+            this.tabPage6.SuspendLayout();
             this.SuspendLayout();
             // 
             // materialTabControl1
@@ -84,6 +103,14 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Vitality";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(161, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 21);
+            this.comboBox1.TabIndex = 5;
             // 
             // testcb
             // 
@@ -197,8 +224,8 @@
             // materialTabControl2
             // 
             this.materialTabControl2.Controls.Add(this.tabPage3);
-            this.materialTabControl2.Controls.Add(this.tabPage5);
             this.materialTabControl2.Controls.Add(this.Info);
+            this.materialTabControl2.Controls.Add(this.tabPage5);
             this.materialTabControl2.Controls.Add(this.tabPage4);
             this.materialTabControl2.Depth = 0;
             this.materialTabControl2.Location = new System.Drawing.Point(-2, 64);
@@ -219,6 +246,78 @@
             this.tabPage3.TabIndex = 0;
             this.tabPage3.Text = "Calculator";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Info
+            // 
+            this.Info.Controls.Add(this.materialTabSelector2);
+            this.Info.Controls.Add(this.materialTabControl3);
+            this.Info.Location = new System.Drawing.Point(4, 22);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(952, 521);
+            this.Info.TabIndex = 3;
+            this.Info.Text = "Item Stats";
+            this.Info.UseVisualStyleBackColor = true;
+            // 
+            // materialListView1
+            // 
+            this.materialListView1.AutoSizeTable = true;
+            this.materialListView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialListView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.PhysicalAbsortion,
+            this.MagicAbsortion,
+            this.FireAbsortion,
+            this.LightningAbsortion,
+            this.DarkAbsortion,
+            this.BleedResistance,
+            this.PoisonResistance,
+            this.FrostResistance,
+            this.CurseResistance,
+            this.Poise,
+            this.Weight,
+            this.PoiseWeightRatio});
+            this.materialListView1.Depth = 0;
+            this.materialListView1.FullRowSelect = true;
+            this.materialListView1.HideSelection = false;
+            this.materialListView1.Location = new System.Drawing.Point(23, 109);
+            this.materialListView1.MinimumSize = new System.Drawing.Size(200, 100);
+            this.materialListView1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialListView1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialListView1.Name = "materialListView1";
+            this.materialListView1.OwnerDraw = true;
+            this.materialListView1.Scrollable = false;
+            this.materialListView1.Size = new System.Drawing.Size(825, 100);
+            this.materialListView1.TabIndex = 2;
+            this.materialListView1.UseCompatibleStateImageBehavior = false;
+            this.materialListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // materialButton2
+            // 
+            this.materialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton2.Depth = 0;
+            this.materialButton2.DrawShadows = true;
+            this.materialButton2.HighEmphasis = true;
+            this.materialButton2.Icon = null;
+            this.materialButton2.Location = new System.Drawing.Point(528, 320);
+            this.materialButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton2.Name = "materialButton2";
+            this.materialButton2.Size = new System.Drawing.Size(59, 36);
+            this.materialButton2.TabIndex = 1;
+            this.materialButton2.Text = "LOAD";
+            this.materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton2.UseAccentColor = false;
+            this.materialButton2.UseVisualStyleBackColor = true;
+            this.materialButton2.Click += new System.EventHandler(this.materialButton2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(116, 48);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(178, 21);
+            this.comboBox2.TabIndex = 0;
             // 
             // tabPage5
             // 
@@ -297,18 +396,6 @@
             this.materialButton1.UseVisualStyleBackColor = true;
             this.materialButton1.Click += new System.EventHandler(this.materialButton1_Click);
             // 
-            // Info
-            // 
-            this.Info.Controls.Add(this.materialTextBox1);
-            this.Info.Controls.Add(this.materialButton2);
-            this.Info.Controls.Add(this.comboBox2);
-            this.Info.Location = new System.Drawing.Point(4, 22);
-            this.Info.Name = "Info";
-            this.Info.Size = new System.Drawing.Size(952, 521);
-            this.Info.TabIndex = 3;
-            this.Info.Text = "Info";
-            this.Info.UseVisualStyleBackColor = true;
-            // 
             // tabPage4
             // 
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
@@ -319,54 +406,107 @@
             this.tabPage4.Text = "About";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // Name
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(161, 179);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(188, 21);
-            this.comboBox1.TabIndex = 5;
+            this.Name.Text = "Name";
+            this.Name.Width = 110;
             // 
-            // comboBox2
+            // PhysicalAbsortion
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(87, 96);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(178, 21);
-            this.comboBox2.TabIndex = 0;
+            this.PhysicalAbsortion.Text = "P";
+            this.PhysicalAbsortion.Width = 45;
             // 
-            // materialButton2
+            // MagicAbsortion
             // 
-            this.materialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.materialButton2.Depth = 0;
-            this.materialButton2.DrawShadows = true;
-            this.materialButton2.HighEmphasis = true;
-            this.materialButton2.Icon = null;
-            this.materialButton2.Location = new System.Drawing.Point(559, 251);
-            this.materialButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialButton2.Name = "materialButton2";
-            this.materialButton2.Size = new System.Drawing.Size(158, 36);
-            this.materialButton2.TabIndex = 1;
-            this.materialButton2.Text = "materialButton2";
-            this.materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.materialButton2.UseAccentColor = false;
-            this.materialButton2.UseVisualStyleBackColor = true;
-            this.materialButton2.Click += new System.EventHandler(this.materialButton2_Click);
+            this.MagicAbsortion.Text = "M";
             // 
-            // materialTextBox1
+            // FireAbsortion
             // 
-            this.materialTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.materialTextBox1.Depth = 0;
-            this.materialTextBox1.Font = new System.Drawing.Font("Roboto", 12F);
-            this.materialTextBox1.Location = new System.Drawing.Point(64, 251);
-            this.materialTextBox1.MaxLength = 50;
-            this.materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
-            this.materialTextBox1.Multiline = false;
-            this.materialTextBox1.Name = "materialTextBox1";
-            this.materialTextBox1.Size = new System.Drawing.Size(466, 50);
-            this.materialTextBox1.TabIndex = 2;
-            this.materialTextBox1.Text = "";
+            this.FireAbsortion.Text = "F";
+            // 
+            // materialTabControl3
+            // 
+            this.materialTabControl3.Controls.Add(this.tabPage6);
+            this.materialTabControl3.Controls.Add(this.tabPage7);
+            this.materialTabControl3.Depth = 0;
+            this.materialTabControl3.Location = new System.Drawing.Point(10, 45);
+            this.materialTabControl3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl3.Name = "materialTabControl3";
+            this.materialTabControl3.SelectedIndex = 0;
+            this.materialTabControl3.Size = new System.Drawing.Size(928, 467);
+            this.materialTabControl3.TabIndex = 3;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.materialButton2);
+            this.tabPage6.Controls.Add(this.materialListView1);
+            this.tabPage6.Controls.Add(this.comboBox2);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(920, 441);
+            this.tabPage6.TabIndex = 0;
+            this.tabPage6.Text = "Armor";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(920, 412);
+            this.tabPage7.TabIndex = 1;
+            this.tabPage7.Text = "Rings";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // materialTabSelector2
+            // 
+            this.materialTabSelector2.BaseTabControl = this.materialTabControl3;
+            this.materialTabSelector2.Depth = 0;
+            this.materialTabSelector2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTabSelector2.Location = new System.Drawing.Point(-4, 0);
+            this.materialTabSelector2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector2.Name = "materialTabSelector2";
+            this.materialTabSelector2.Size = new System.Drawing.Size(953, 39);
+            this.materialTabSelector2.TabIndex = 4;
+            this.materialTabSelector2.Text = "materialTabSelector2";
+            // 
+            // LightningAbsortion
+            // 
+            this.LightningAbsortion.Text = "L";
+            // 
+            // DarkAbsortion
+            // 
+            this.DarkAbsortion.Text = "D";
+            // 
+            // BleedResistance
+            // 
+            this.BleedResistance.Text = "B";
+            this.BleedResistance.Width = 70;
+            // 
+            // PoisonResistance
+            // 
+            this.PoisonResistance.Text = "Po";
+            // 
+            // FrostResistance
+            // 
+            this.FrostResistance.Text = "Fr";
+            // 
+            // CurseResistance
+            // 
+            this.CurseResistance.Text = "Cu";
+            // 
+            // Poise
+            // 
+            this.Poise.Text = "Poise";
+            // 
+            // Weight
+            // 
+            this.Weight.Text = "W";
+            // 
+            // PoiseWeightRatio
+            // 
+            this.PoiseWeightRatio.Text = "P/W";
             // 
             // Form1
             // 
@@ -375,7 +515,6 @@
             this.ClientSize = new System.Drawing.Size(952, 610);
             this.Controls.Add(this.materialTabControl2);
             this.DrawerTabControl = this.materialTabControl2;
-            this.Name = "Form1";
             this.Text = "DS3 Build Planner";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.materialTabControl1.ResumeLayout(false);
@@ -383,10 +522,12 @@
             this.tabPage1.PerformLayout();
             this.materialTabControl2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
+            this.Info.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
             this.tabPage5.PerformLayout();
-            this.Info.ResumeLayout(false);
-            this.Info.PerformLayout();
+            this.materialTabControl3.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -413,7 +554,24 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private MaterialSkin.Controls.MaterialButton materialButton2;
-        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
+        private MaterialSkin.Controls.MaterialListView materialListView1;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader PhysicalAbsortion;
+        private System.Windows.Forms.ColumnHeader MagicAbsortion;
+        private System.Windows.Forms.ColumnHeader FireAbsortion;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector2;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl3;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.ColumnHeader LightningAbsortion;
+        private System.Windows.Forms.ColumnHeader DarkAbsortion;
+        private System.Windows.Forms.ColumnHeader BleedResistance;
+        private System.Windows.Forms.ColumnHeader PoisonResistance;
+        private System.Windows.Forms.ColumnHeader FrostResistance;
+        private System.Windows.Forms.ColumnHeader CurseResistance;
+        private System.Windows.Forms.ColumnHeader Poise;
+        private System.Windows.Forms.ColumnHeader Weight;
+        private System.Windows.Forms.ColumnHeader PoiseWeightRatio;
     }
 }
 
