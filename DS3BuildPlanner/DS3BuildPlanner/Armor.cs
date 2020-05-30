53fdf116767820a29b57e49a4d308e4f708e39f2
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-30 14:20:06 +0200

    Update Armor.cs

--- DS3BuildPlanner/DS3BuildPlanner/Armor.cs
+++ DS3BuildPlanner/DS3BuildPlanner/Armor.cs
@@ -9,18 +9,18 @@ namespace DS3BuildPlanner
     class Armor
     {
         private String name;
-        private float PhysicalAbsortion ;
-        private float MagicAbsortion;
-        private float FireAbsortion;
-        private float LightningAbsortion;
-        private float DarkAbsortion;
-        private int BleedResistance;
-        private int PoisonResistance;
-        private int FrostResistance;
-        private int CurseResistance;
-        private float Poise;
-        private float Weight;
-        private float PoiseWeightRatio;
+        private float physicalAbsortion ;
+        private float magicAbsortion;
+        private float fireAbsortion;
+        private float lightningAbsortion;
+        private float darkAbsortion;
+        private int bleedResistance;
+        private int poisonResistance;
+        private int frostResistance;
+        private int curseResistance;
+        private float poise;
+        private float weight;
+        private float poiseWeightRatio;
 
         public Armor()
         {
@@ -29,20 +29,33 @@ namespace DS3BuildPlanner
         public Armor(string name, float physicalAbsortion, float magicAbsortion, float fireAbsortion, float lightningAbsortion, float darkAbsortion, int bleedResistance, int poisonResistance, int frostResistance, int curseResistance, float poise, float weight, float poiseWeightRatio)
         {
             this.name = name;
-            PhysicalAbsortion = physicalAbsortion;
-            MagicAbsortion = magicAbsortion;
-            FireAbsortion = fireAbsortion;
-            LightningAbsortion = lightningAbsortion;
-            DarkAbsortion = darkAbsortion;
-            BleedResistance = bleedResistance;
-            PoisonResistance = poisonResistance;
-            FrostResistance = frostResistance;
-            CurseResistance = curseResistance;
-            Poise = poise;
-            Weight = weight;
-            PoiseWeightRatio = poiseWeightRatio;
+            this.physicalAbsortion = physicalAbsortion;
+            this.magicAbsortion = magicAbsortion;
+            this.fireAbsortion = fireAbsortion;
+            this.lightningAbsortion = lightningAbsortion;
+            this.darkAbsortion = darkAbsortion;
+            this.bleedResistance = bleedResistance;
+            this.poisonResistance = poisonResistance;
+            this.frostResistance = frostResistance;
+            this.curseResistance = curseResistance;
+            this.poise = poise;
+            this.weight = weight;
+            this.poiseWeightRatio = poiseWeightRatio;
         }
 
+        //Getter & Setters
         public String Name { get { return name; } set { name = value; } }
+        public float PhysicalAbsortion { get; set; }
+        public float MagicAbsortion { get { return this.magicAbsortion; } set { this.magicAbsortion = value; } }
+        public float FireAbsortion { get { return fireAbsortion; } set { fireAbsortion = value; } }
+        public float LightningAbsortion { get { return lightningAbsortion; } set { lightningAbsortion = value; } }
+        public float DarkAbsortion { get { return darkAbsortion; } set { darkAbsortion = value; } }
+        public int BleedResistance { get { return bleedResistance; } set { bleedResistance = value; } }
+        public int PoisonResistance { get { return poisonResistance; } set { poisonResistance = value; } }
+        public int FrostResistance { get { return frostResistance; } set { frostResistance = value; } }
+        public int CurseResistance { get { return curseResistance; } set { curseResistance = value; } }
+        public float Poise { get { return poise; } set { poise = value; } }
+        public float Weight { get { return weight; } set { weight = value; } }
+        public float PoiseWeightRatio { get { return poiseWeightRatio; } set { poiseWeightRatio = value; } }
     }
 }
