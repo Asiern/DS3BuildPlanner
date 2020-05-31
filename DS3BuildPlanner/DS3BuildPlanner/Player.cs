9f6768016abb7bc1f28ab8d66709e61789199b88
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-31 11:22:50 +0200

    Update Player.cs

--- DS3BuildPlanner/DS3BuildPlanner/Player.cs
+++ DS3BuildPlanner/DS3BuildPlanner/Player.cs
@@ -60,5 +60,135 @@ namespace DS3BuildPlanner
         {
 
         }
+
+        public void setupPlayer(String startingClass)
+        {
+            if (this.startingClass != startingClass) this.startingClass = startingClass;
+
+
+            switch (startingClass)
+            {
+                case "Knight":
+                    this.level = 9;
+                    this.vigor = 12;
+                    this.attunement = 10;
+                    this.endurance = 11;
+                    this.vitality = 15;
+                    this.strenght = 13;
+                    this.dexterity = 12;
+                    this.intelligence = 9;
+                    this.faith = 9;
+                    this.luck = 7;
+                    break;
+                case "Mercenary":
+                    this.level = 8;
+                    this.vigor = 11;
+                    this.attunement = 12;
+                    this.endurance = 11;
+                    this.vitality = 10;
+                    this.strenght = 10;
+                    this.dexterity = 16;
+                    this.intelligence = 10;
+                    this.faith = 8;
+                    this.luck = 9;
+                    break;
+                case "Warrior":
+                    this.level = 7;
+                    this.vigor = 14;
+                    this.attunement = 6;
+                    this.endurance = 12;
+                    this.vitality = 11;
+                    this.strenght = 16;
+                    this.dexterity = 9;
+                    this.intelligence = 8;
+                    this.faith = 9;
+                    this.luck = 11;
+                    break;
+                case "Herald":
+                    this.level = 9;
+                    this.vigor = 12;
+                    this.attunement = 10;
+                    this.endurance = 9;
+                    this.vitality = 12;
+                    this.strenght = 12;
+                    this.dexterity = 11;
+                    this.intelligence = 8;
+                    this.faith = 13;
+                    this.luck = 11;
+                    break;
+                case "Thief":
+                    this.level = 5;
+                    this.vigor = 10;
+                    this.attunement = 11;
+                    this.endurance = 10;
+                    this.vitality = 9;
+                    this.strenght = 9;
+                    this.dexterity = 13;
+                    this.intelligence = 10;
+                    this.faith = 8;
+                    this.luck = 14;
+                    break;
+                case "Assassin":
+                    this.level = 10;
+                    this.vigor = 10;
+                    this.attunement = 14;
+                    this.endurance = 11;
+                    this.vitality = 10;
+                    this.strenght = 10;
+                    this.dexterity = 14;
+                    this.intelligence = 11;
+                    this.faith = 9;
+                    this.luck = 10;
+                    break;
+                case "Sorcerer":
+                    this.level = 6;
+                    this.vigor = 9;
+                    this.attunement = 16;
+                    this.endurance = 9;
+                    this.vitality = 7;
+                    this.strenght = 7;
+                    this.dexterity = 12;
+                    this.intelligence = 16;
+                    this.faith = 7;
+                    this.luck = 12;
+                    break;
+                case "Pyromancer":
+                    this.level = 8;
+                    this.vigor = 11;
+                    this.attunement = 12;
+                    this.endurance = 10;
+                    this.vitality = 18;
+                    this.strenght = 12;
+                    this.dexterity = 9;
+                    this.intelligence = 14;
+                    this.faith = 14;
+                    this.luck = 7;
+                    break;
+                case "Cleric":
+                    this.level = 7;
+                    this.vigor = 10;
+                    this.attunement = 14;
+                    this.endurance = 9;
+                    this.vitality = 7;
+                    this.strenght = 12;
+                    this.dexterity = 8;
+                    this.intelligence = 7;
+                    this.faith = 16;
+                    this.luck = 13;
+                    break;
+                case "Deprived":
+                    this.level = 1;
+                    this.vigor = 10;
+                    this.attunement = 10;
+                    this.endurance = 10;
+                    this.vitality = 10;
+                    this.strenght = 10;
+                    this.dexterity = 10;
+                    this.intelligence = 10;
+                    this.faith = 10;
+                    this.luck = 10;
+                    break;
+            }
+        }
     }
 }
