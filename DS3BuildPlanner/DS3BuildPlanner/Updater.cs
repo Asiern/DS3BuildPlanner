7ec8845a4a1fa624fbf60f1e94a18f7bb3da0a37
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-31 10:54:36 +0200

    Create Updater.cs

---
+++ DS3BuildPlanner/DS3BuildPlanner/Updater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS3BuildPlanner
+{
+    class Updater
+    {
+        public Updater()
+        {
+
+        }
+        public void update() { 
+            WebClient webClient = new WebClient();
+            if (!webClient.DownloadString("https://pastebin.com/raw/4SDWV9m4").Contains("1.0"))
+            {
+                if (MessageBox.Show("Update available", "DS3BuildPlanner Updater", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process.Start("https://github.com/Asiern/DS3BuildPlanner/releases");
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        public string getVersion()
+        {
+            WebClient webClient = new WebClient();
+            return webClient.DownloadString("https://pastebin.com/raw/4SDWV9m4");
+        }
+    }
+}
