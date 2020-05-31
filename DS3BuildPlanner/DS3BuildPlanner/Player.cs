91555418b8fbd2157ef85e90c5f0b6246a7c5ac3
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-31 11:06:28 +0200

    Update Player.cs

--- DS3BuildPlanner/DS3BuildPlanner/Player.cs
+++ DS3BuildPlanner/DS3BuildPlanner/Player.cs
@@ -8,6 +8,54 @@ namespace DS3BuildPlanner
 {
     class Player
     {
+        private string startingClass;
+        private int level;
+        private int souls;
+
+        //BASE POWER
+        private int hp;
+        private int fp;
+        private int stamina;
+        private float equipLoad;
+        private float poise;
+        private int itemDiscovery;
+
+        //ATTACK POWER
+        private int rWeapon1;
+        private int rWeapon2;
+        private int rWeapon3;
+        private int lWeapon1;
+        private int lWeapon2;
+        private int lWeapon3;
+
+        //DEFENSE
+        private int physicalDefense;
+        private int magicDefense;
+        private int fireDefense;
+        private int lightningDefense;
+        private int darkDefense;
+
+        //RESISTANCES
+        private int bleedResistance;
+        private int poisonResistance;
+        private int frostResistance;
+        private int curseResistance;
+
+        //SLOTS
+        private int attunementSlots;
+
+        //ATTRIBUTES
+        private int vigor;
+        private int attunement;
+        private int endurance;
+        private int vitality;
+        private int strenght;
+        private int dexterity;
+        private int intelligence;
+        private int faith;
+        private int luck;
+
+
         public Player()
         {
 
