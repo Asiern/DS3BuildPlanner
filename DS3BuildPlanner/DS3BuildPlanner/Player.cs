8e737d02aedc841a7a1a1cef77e9e16f416a26af
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-01 20:19:54 +0200

    Update Player.cs

--- DS3BuildPlanner/DS3BuildPlanner/Player.cs
+++ DS3BuildPlanner/DS3BuildPlanner/Player.cs
@@ -45,6 +45,18 @@ namespace DS3BuildPlanner
         //SLOTS
         private int attunementSlots;
 
+        //CLASS ATTRIBUTES
+        private int baselevel;
+        private int basevigor;
+        private int baseattunement;
+        private int baseendurance;
+        private int basevitality;
+        private int basestrenght;
+        private int basedexterity;
+        private int baseintelligence;
+        private int basefaith;
+        private int baseluck;
+
         //ATTRIBUTES
         private int vigor;
         private int attunement;
@@ -57,6 +69,46 @@ namespace DS3BuildPlanner
         private int luck;
 
         //GETTERS
+        public int getbaseVigor()
+        {
+            return this.basevigor;
+        }
+        public int getbaseAttunement()
+        {
+            return this.baseattunement;
+        }
+        public int getbaseEndurance()
+        {
+            return this.baseendurance;
+        }
+        public int getbaseVitality()
+        {
+            return this.basevitality;
+        }
+        public int getbaseStrenght()
+        {
+            return this.basestrenght;
+        }
+        public int getbaseDexterity()
+        {
+            return this.basedexterity;
+        }
+        public int getbaseIntelligence()
+        {
+            return this.baseintelligence;
+        }
+        public int getbaseFaith()
+        {
+            return this.basefaith;
+        }
+        public int getbaseLuck()
+        {
+            return this.baseluck;
+        }
+        public int getbaseLevel()
+        {
+            return this.baselevel;
+        }
         public int getVigor()
         {
             return this.vigor;
@@ -102,7 +154,7 @@ namespace DS3BuildPlanner
 
         public void setVigor(int v)
         {
-            this.vigor = v;
+            this.vigor = v;       
         }
         public void setAttunement(int a)
         {
@@ -203,6 +255,17 @@ namespace DS3BuildPlanner
                     this.intelligence = 9;
                     this.faith = 9;
                     this.luck = 7;
+                    this.baselevel = 9;
+                    this.basevigor = 12;
+                    this.baseattunement = 10;
+                    this.baseendurance = 11;
+                    this.basevitality = 15;
+                    this.basestrenght = 13;
+                    this.basedexterity = 12;
+                    this.baseintelligence = 9;
+                    this.basefaith = 9;
+                    this.baseluck = 7;
+
                     break;
                 case "Mercenary":
                     this.level = 8;
@@ -215,6 +278,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 10;
                     this.faith = 8;
                     this.luck = 9;
+                    this.baselevel = 8;
+                    this.basevigor = 11;
+                    this.baseattunement = 12;
+                    this.baseendurance = 11;
+                    this.basevitality = 10;
+                    this.basestrenght = 10;
+                    this.basedexterity = 16;
+                    this.baseintelligence = 10;
+                    this.basefaith = 8;
+                    this.baseluck = 9;
                     break;
                 case "Warrior":
                     this.level = 7;
@@ -227,6 +300,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 8;
                     this.faith = 9;
                     this.luck = 11;
+                    this.baselevel = 7;
+                    this.basevigor = 14;
+                    this.baseattunement = 6;
+                    this.baseendurance = 12;
+                    this.basevitality = 11;
+                    this.basestrenght = 16;
+                    this.basedexterity = 9;
+                    this.baseintelligence = 8;
+                    this.basefaith = 9;
+                    this.baseluck = 11;
                     break;
                 case "Herald":
                     this.level = 9;
@@ -239,6 +322,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 8;
                     this.faith = 13;
                     this.luck = 11;
+                    this.baselevel = 9;
+                    this.basevigor = 12;
+                    this.baseattunement = 10;
+                    this.baseendurance = 9;
+                    this.basevitality = 12;
+                    this.basestrenght = 12;
+                    this.basedexterity = 11;
+                    this.baseintelligence = 8;
+                    this.basefaith = 13;
+                    this.baseluck = 11;
                     break;
                 case "Thief":
                     this.level = 5;
@@ -251,6 +344,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 10;
                     this.faith = 8;
                     this.luck = 14;
+                    this.baselevel = 5;
+                    this.basevigor = 10;
+                    this.baseattunement = 11;
+                    this.baseendurance = 10;
+                    this.basevitality = 9;
+                    this.basestrenght = 9;
+                    this.basedexterity = 13;
+                    this.baseintelligence = 10;
+                    this.basefaith = 8;
+                    this.baseluck = 14;
                     break;
                 case "Assassin":
                     this.level = 10;
@@ -263,6 +366,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 11;
                     this.faith = 9;
                     this.luck = 10;
+                    this.baselevel = 10;
+                    this.basevigor = 10;
+                    this.baseattunement = 14;
+                    this.baseendurance = 11;
+                    this.basevitality = 10;
+                    this.basestrenght = 10;
+                    this.basedexterity = 14;
+                    this.baseintelligence = 11;
+                    this.basefaith = 9;
+                    this.baseluck = 10;
                     break;
                 case "Sorcerer":
                     this.level = 6;
@@ -275,6 +388,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 16;
                     this.faith = 7;
                     this.luck = 12;
+                    this.baselevel = 6;
+                    this.basevigor = 9;
+                    this.baseattunement = 16;
+                    this.baseendurance = 9;
+                    this.basevitality = 7;
+                    this.basestrenght = 7;
+                    this.basedexterity = 12;
+                    this.baseintelligence = 16;
+                    this.basefaith = 7;
+                    this.baseluck = 12;
                     break;
                 case "Pyromancer":
                     this.level = 8;
@@ -287,6 +410,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 14;
                     this.faith = 14;
                     this.luck = 7;
+                    this.baselevel = 8;
+                    this.basevigor = 11;
+                    this.baseattunement = 12;
+                    this.baseendurance = 10;
+                    this.basevitality = 18;
+                    this.basestrenght = 12;
+                    this.basedexterity = 9;
+                    this.baseintelligence = 14;
+                    this.basefaith = 14;
+                    this.baseluck = 7;
                     break;
                 case "Cleric":
                     this.level = 7;
@@ -299,6 +432,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 7;
                     this.faith = 16;
                     this.luck = 13;
+                    this.baselevel = 7;
+                    this.basevigor = 10;
+                    this.baseattunement = 14;
+                    this.baseendurance = 9;
+                    this.basevitality = 7;
+                    this.basestrenght = 12;
+                    this.basedexterity = 8;
+                    this.baseintelligence = 7;
+                    this.basefaith = 16;
+                    this.baseluck = 13;
                     break;
                 case "Deprived":
                     this.level = 1;
@@ -311,6 +454,16 @@ namespace DS3BuildPlanner
                     this.intelligence = 10;
                     this.faith = 10;
                     this.luck = 10;
+                    this.baselevel = 1;
+                    this.basevigor = 10;
+                    this.baseattunement = 10;
+                    this.baseendurance = 10;
+                    this.basevitality = 10;
+                    this.basestrenght = 10;
+                    this.basedexterity = 10;
+                    this.baseintelligence = 10;
+                    this.basefaith = 10;
+                    this.baseluck = 10;
                     break;
             }
         }
